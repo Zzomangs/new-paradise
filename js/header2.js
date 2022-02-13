56d0204296442ad1d5c98c50a223c79fd32d2764
Ye Reong (Yeye) <92131966+Zzomangs@users.noreply.github.com>
2022-02-13 21:54:41 +0900

    Create header2.js

---
+++ js/header2.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+
+
+  // 대메뉴 ul에 마우스 올릴때
+  // 메뉴배경 보이게
+  // $(".header-m").mouseenter(function(){
+  //   $(".main-navbg").stop().css({height:"0px"}).animate({height:"400px"},1000)
+  // });
+
+  //대메뉴 ul의 li에 마우스 올릴때 다른 소메뉴뜨게하기
+
+  $('.header-m > li').mouseenter(function(){
+    $('.header-m > li').removeClass('on'); 
+    $('.main-nav').css({height:"0px"});
+     
+    $(this).addClass('on');
+    var i = $(this).index();
+    $('.main-nav').eq(i).css({height:"0px"}).animate({height:'400px'});
+   
+  });
+  
+
+  $('.main-nav').mouseleave(function(){
+     
+   $('.header-m > li').removeClass('on');
+   $(this).css({height:"400px"}).animate({height:'0px'});       
+  
+  });
+
+
+  // $(".header-m > li").mouseenter(function(){
+  //   $(this).next(".main-nav").addClass("active")
+  // });
+  // li 의 형제로 마우스를 올렸을때 기존의 .main-nav의 active를 제거하고 
+  //새롭게 보이게 할 수 는 없을까요? 
+  // 마우스가 올리는 li 마다 다른 내용의 main-nav가 active되어야합니다ㅠ
+  // $(".header-m > li").mouseleave(function(){
+  //   $(this).siblings(".main-nav").removeClass("active")
+  // });
+
+  // 메뉴배경 사라지게
+  // $(".header-m > .main-nav").mouseleave(function(){
+  //   $(".main-nav").removeClass("active")
+  //   $(".main-navbg").css({height:"400px"}).animate({height:"0px"},500)
+  // });
+  
+ //헤더 언어 토글링
+ $("#kr").on("click",function(){
+   $("#leng").toggleClass('show')
+ })
+
+
+
+
+ 
+});
